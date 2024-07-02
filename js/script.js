ea02e5ecdeb46875c54230f247d5056e6cb895bd
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-02 15:05:28 +0200

    While index declared and unordered list created

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ const listaDellaSpesa = ['Pane', 'Latte', 'Uova', 'Frutta', 'Verdura'];
 
 // Elemento HTML dove verrà visualizzata la lista della spesa
 const listaSpesaElement = document.getElementById('listaSpesa');
+
+// Inizializzazione dell'indice per il ciclo while
+let i = 0;
+let listaHtml = '<ul>'; // Inizia la lista non ordinata
