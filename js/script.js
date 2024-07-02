2fb71231e9d26696fcaad790976eb02dded1ee55
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-02 15:04:20 +0200

    Array linked to div into index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 // Array degli elementi della lista della spesa
 const listaDellaSpesa = ['Pane', 'Latte', 'Uova', 'Frutta', 'Verdura'];
 
+// Elemento HTML dove verrà visualizzata la lista della spesa
+const listaSpesaElement = document.getElementById('listaSpesa');
